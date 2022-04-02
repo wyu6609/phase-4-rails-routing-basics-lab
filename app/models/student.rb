14517de2987e0047d46b9590706a501b547b0d8e
William Yu <wy427@nyu.edu>
2022-04-02 12:59:45 -0400

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,17 @@
 class Student < ApplicationRecord
-
   def to_s
     "#{first_name} #{last_name}"
   end
 
+  def first_name
+    self.first_name
+  end
+
+  def last_name
+    self.last_name
+  end
+
+  def grade
+    self.grade
+  end
 end
